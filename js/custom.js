28368fb0c662b720a97e00a310ff2242feb92054
Jason Wuerch <wuerchjason@gmail.com>
2016-09-26 18:18:04 -0700

    add in js for toggle function on mobile menu

--- js/custom.js
+++ js/custom.js
@@ -51,5 +51,10 @@
             $('.phone-slug > div').addClass('container');
         }
     });
+    $('.mobile-primary-menu a').click(function () {
+        if ($('#primary-menu').find('ul.mobile-primary-menu').length > 0) {
+            $('ul.mobile-primary-menu').toggleClass("show");
+        }
+    });
 
 })(jQuery);
